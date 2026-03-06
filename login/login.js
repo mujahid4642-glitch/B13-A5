2001b986b7a5c042dafc76835e54a9c0d16bbc7d
mujahid4642-glitch <mujahid4642@gamil.com>
2026-03-07 01:06:55 +0600

    first commit

---
+++ login/login.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("login-btn").addEventListener("click",()=>{
+    // 1.get the username
+    // 2.get the password
+    // 3.match the username & password
+    // 4.true>>>alert> home page
+    // 5.false>>alert>return
+})
