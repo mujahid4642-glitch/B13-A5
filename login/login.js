66519aa0499d972d2b3a4475360d78cad64a8e3e
mujahid4642-glitch <mujahid4642@gamil.com>
2026-03-07 16:22:27 +0600

    navber ready

--- login/login.js
+++ login/login.js
@@ -2,8 +2,23 @@
 
 document.getElementById("login-btn").addEventListener("click",()=>{
     // 1.get the username
+    const nameInput=document.getElementById("name-input")
+    const username = nameInput.value 
+    // console.log(username);
     // 2.get the password
+    const inputPass =document.getElementById("input-pass")
+    const password = inputPass.value
+    console.log(password);
     // 3.match the username & password
-    // 4.true>>>alert> home page
-    // 5.false>>alert>return
-})
+        if (username === "admin" && password === "admin123") {
+        // 4.true>>>alert> home page
+        alert("entry");
+        window.location.assign("/home.html")
+    } else {
+        // 5.false>>alert>return
+        alert("no entry");
+        return
+    }
+});
+    
+    
